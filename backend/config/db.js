4002d499ee583af034084d1c0bed8aab8c05e522
gnahc2305 <achangmurillo@gmail.com>
2023-02-13 19:27:53 -0500

    connected with mongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect()
-    } catch (error) {
-        
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+    connectDB,
 }
