87228ea8ac7060ba5400a7569eaace06b7c4e831
gnahc2305 <achangmurillo@gmail.com>
2023-02-23 21:18:28 -0500

    create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
